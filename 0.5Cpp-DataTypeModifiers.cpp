e92052ccae599aa42eb0fbc6b98220e395920b57
Shivam Dangwal <shivamdangwal.1585@gmail.com>
2025-07-12 14:58:01 +0530

    DataTypeModifiers

---
+++ 0.5Cpp-DataTypeModifiers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Data Type Modifiers in C++
+    // long -> >=4 bytes
+    long int a = 1234567890;
+    // long long -> >=8 bytes
+    long long int b = 1234567890123456789;
+    // short -> >=2 bytes
+    short int c = 12345;
+    // signed -> can be negative
+    signed int d = -12345;
+    // unsigned -> cannot be negative
+    unsigned int e = 12345;
+    return 0;
+}
